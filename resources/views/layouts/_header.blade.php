7525d2e72a2bec677a5e8d660354aaba92b52382
ljheisenberg1072 <ljheisenberg@163.com>
2019-05-09 07:37:27 +0000

    注册与登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
-        <!-- Branding Image -->
         <a href="{{ url('/') }}" class="navbar-brand">
             CAPINNO
         </a>
@@ -8,14 +7,28 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
 
             </ul>
-            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav navbar-right">
-                <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">注册</a></li>
+                <!-- 登录注册开始 -->
+                @guest
+                <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
+                <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+                @else
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                        {{ Auth::user()->name  }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="#" class="dropdown-item" id="logout" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+                        <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                </li>
+                @endguest
             </ul>
         </div>
     </div>
